3e6c214a8043e88d3235c256a7de4774cf95a43e
msabic <mijo.sabic@student.fst.ba>
2018-09-24 23:46:24 +0200

    commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <v-app><router-view></router-view></v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false,
+      // drawer: true,
+      items: [
+        {title: 'Active notes', icon: 'style', link: '/'},
+        {title: 'Test', icon: 'delete_sweep', link: '/test'}
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/Test.vue
@@ -0,0 +1,3 @@
+<template>
+<p>fasdfasdf</p>
+</template>
