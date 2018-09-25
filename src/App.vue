131230ce0773d74a63d29247488c8d5d3f841bea
msabic <mijo.sabic@student.fsr.ba>
2018-09-25 14:10:27 +0200

    login

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
 <template>
- <v-app><router-view></router-view></v-app>
+ <v-app>
+   <v-card class="hide-overflow" height="200px">
+      <v-card-text class="text-xs-center">
+      <v-btn
+        flat
+        color="primary"
+        @click="showNav = !showNav"
+      >
+        Toggle Nav
+      </v-btn>
+    </v-card-text>
+    <v-bottom-nav
+      :active.sync="activeBtn"
+      :value="showNav"
+      absolute
+      color="transparent"
+    >
+      <v-btn flat color="teal">
+        <span>Recents</span>
+        <v-icon>history</v-icon>
+      </v-btn>
+
+      <v-btn flat color="teal">
+        <span>Favorites</span>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn flat color="teal">
+        <span>Nearby</span>
+        <v-icon>place</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+   </v-card>
+   <router-view></router-view>
+   </v-app>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      sideNav: false,
-      // drawer: true,
+  sideNav: false,
+  // drawer: true,
       items: [
         {title: 'Active notes', icon: 'style', link: '/'},
         {title: 'Test', icon: 'delete_sweep', link: '/test'}
@@ -16,3 +50,6 @@ export default {
   }
 }
 </script>
+
+<style>
+</style>
