0d8cc6cfa78f1946f88a5bfa2f912b8abd6200f1
msabic <mijo.sabic@student.fsr.ba>
2018-10-02 09:00:35 +0200

    menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer v-model="sideNav">
+    <v-navigation-drawer v-model="sideNav" fix app>
       <v-list>
         <v-list-tile>
           <v-list-tile-action>
@@ -44,3 +44,71 @@ export default {
 
   @import './stylus/main'
 </style>
+
+<!--<template>
+ <v-card
+    color="grey lighten-4"
+    flat
+    height="200px"
+  >
+  <v-navigation-drawer v-model="sideNav" fixed app>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>gavel</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="grey darken-1" dark>
+      <v-toolbar-side-icon  @click.stop="sideNav = !sideNav"
+        class="hidden-sm-and-up "></v-toolbar-side-icon>
+
+      <v-menu :nudge-width="100">
+        <v-toolbar-title slot="activator">
+          <span>All</span>
+          <v-icon dark>arrow_drop_down</v-icon>
+        </v-toolbar-title>
+
+        <v-list>
+          <v-list-tile
+            v-for="item in items"
+            :key="item"
+             @click="foo">
+            <v-list-tile-title v-text="item"></v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    sideNav: false,
+    items: [
+      'All', 'Family', 'Friends', 'Coworkers'
+    ]
+  })
+}
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>-->
